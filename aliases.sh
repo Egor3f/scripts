707a498b1847b77fd7abce771bcdbc7a677e558a
Egor Aristov <ef@efprojects.com>
2025-05-06 16:42:57 +0300

    aliases

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,3 @@
 git config --global alias.pushall '!git remote | xargs -L1 git push'
 alias mountnc=~/Dev/scripts/mountnc.sh
+alias gitlog='git log --graph --pretty=oneline --abbrev-commit --all --decorate'
